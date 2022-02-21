1078b08e43fa6ac15a039822278ad0b5c0a44679
Taufik Ridho <taufikridho505@gmail.com>
2022-02-21 16:06:31 +0700

    add interactive mobile nav

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+function nav(){
+    var x = document.getElementById("bar");
+
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
